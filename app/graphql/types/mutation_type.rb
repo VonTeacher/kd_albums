f59e2dfbc7b6de3ea6a0c1cf3f6a77c5ec2b1ecf
Vaughn Weiss <vaughn.weiss@gmail.com>
2022-08-13 17:09:04 -0600

    Create User type, add to mutation, create user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_artist, mutation: Mutations::CreateArtist
     field :create_album, mutation: Mutations::CreateAlbum
+    field :create_user, mutation: Mutations::CreateUser
   end
 end
