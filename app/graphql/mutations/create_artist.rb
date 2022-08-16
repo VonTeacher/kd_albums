c8ed5ab22125460fa245502fb8c11034fecb2932
Vaughn Weiss <vaughn.weiss@gmail.com>
2022-08-15 18:48:43 -0600

    Refactor to easier to read module format

--- app/graphql/mutations/create_artist.rb
+++ app/graphql/mutations/create_artist.rb
@@ -14,19 +14,21 @@
 #   }
 # }
 
-class Mutations::CreateArtist < Mutations::BaseMutation
-  argument :name, String, required: true
+module Mutations
+  class CreateArtist < Mutations::BaseMutation
+    argument :name, String, required: true
 
-  field :artist, Types::ArtistType, null: false
-  field :errors, [String], null: false
+    field :artist, Types::ArtistType, null: false
+    field :errors, [String], null: false
 
-  def resolve(name:)
-    artist = Artist.new(name: name)
+    def resolve(name:)
+      artist = Artist.new(name: name)
 
-    if artist.save
-      { artist: artist, errors: [] }
-    else
-      { artist: nil, errors: artist.errors.full_messages }
+      if artist.save
+        { artist: artist, errors: [] }
+      else
+        { artist: nil, errors: artist.errors.full_messages }
+      end
     end
   end
 end
