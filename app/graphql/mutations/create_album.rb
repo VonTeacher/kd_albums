b66638b967f3df244dadbce02a8287a49e5e09f8
Vaughn Weiss <17757205+VonTeacher@users.noreply.github.com>
2022-08-15 18:50:49 -0600

    Refactor to easier to read module format (#15)

--- app/graphql/mutations/create_album.rb
+++ app/graphql/mutations/create_album.rb
@@ -17,27 +17,28 @@
 #   }
 # }
 
+module Mutations
+  class CreateAlbum < Mutations::BaseMutation
+    argument :title, String, required: true
+    argument :artist, String, required: true
+    argument :user, String, required: true
 
-class Mutations::CreateAlbum < Mutations::BaseMutation
-  argument :title, String, required: true
-  argument :artist, String, required: true
-  argument :user, String, required: true
+    field :album, Types::AlbumType, null: false
+    field :user, Types::UserType, null: false
+    field :errors, [String], null: false
 
-  field :album, Types::AlbumType, null: false
-  field :user, Types::UserType, null: false
-  field :errors, [String], null: false
+    def resolve(title:, artist:, user:)
+      album = Album.new(
+        title: title,
+        artist: Artist.find_by(name: artist),
+        user: User.find_by(name: user)
+      )
 
-  def resolve(title:, artist:, user:)
-    album = Album.new(
-      title: title,
-      artist: Artist.find_by(name: artist),
-      user: User.find_by(name: user)
-    )
-
-    if album.save
-      { album: album, errors: [] }
-    else
-      { album: nil, errors: album.errors.full_messages }
+      if album.save
+        { album: album, errors: [] }
+      else
+        { album: nil, errors: album.errors.full_messages }
+      end
     end
   end
 end
